911f1fe07fc8a03556f5d0b3c7ae3a64cdde1bf0
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2020-09-03 17:31:53 +0300

    SCons: Allow to build Goost out of the same directory
    Added root `SConstruct` which is a convenience/compatibility script
    allowing to build Goost in the same way as building Godot Engine
    so the usage should be the same.
    
    Internally, this attempts to find Godot source and build the engine
    with the Goost module, also providing customization such as
    appending extra suffix to the resulting binaries and overriding
    engine build name.
    
    This also allows to build Goost with the existing built-in and community
    `modules/`, which was pretty cumbersome to do (previously, you had to
    manually append to the `custom_modules` option). But both approaches
    are supported.

--- version.py
+++ version.py
@@ -1,7 +1,11 @@
 short_name = "goost"
 name = "Goost: Godot Engine Extension"
+url = "https://github.com/goostengine/goost"
 major = 0
 minor = 1
 patch = 0
 status = "dev"
 year = 2020
+
+godot_version = "3.2"
+godot_url = "https://github.com/godotengine/godot"
