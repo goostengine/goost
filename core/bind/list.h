937da92bb808e7d20e92f0be41f81e9dd2e2d8ed
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2020-09-10 19:26:34 +0300

    Teach `Variant` to convert `VariantListElement`

--- core/bind/list.h
+++ core/bind/list.h
@@ -11,42 +11,42 @@
 class VariantListElement;
 
 struct VariantListData {
-    VariantListElement *first = nullptr;
-    VariantListElement *last = nullptr;
-    int size_cache = 0;
-    bool erase(VariantListElement *p_I);
+	VariantListElement *first = nullptr;
+	VariantListElement *last = nullptr;
+	int size_cache = 0;
+	bool erase(VariantListElement *p_I);
 };
 
 class VariantListElement : public Object {
-    GDCLASS(VariantListElement, Object);
-    
-protected:
-    static void _bind_methods();
-    
-private:
-    friend class VariantList;
-    friend struct VariantListData;
+	GDCLASS(VariantListElement, Object);
 
-    Variant value;
-    VariantListElement *next_ptr = nullptr;
-    VariantListElement *prev_ptr = nullptr;
-    VariantListData *data = nullptr;
+protected:
+	static void _bind_methods();
+
+private:
+	friend class VariantList;
+	friend struct VariantListData;
+
+	Variant value;
+	VariantListElement *next_ptr = nullptr;
+	VariantListElement *prev_ptr = nullptr;
+	VariantListData *data = nullptr;
 
 public:
-    VariantListElement *next();
-    VariantListElement *prev();
-    Variant get_value() { return value; }
-    void set_value(const Variant &p_value) { value = p_value; }
-    void erase() { data->erase(this); }
+	VariantListElement *next();
+	VariantListElement *prev();
+	Variant get_value() { return value; }
+	void set_value(const Variant &p_value) { value = p_value; }
+	void erase() { data->erase(this); }
 
-    VariantListElement() {}
+	VariantListElement() {}
 };
 
 class VariantList : public Object {
-    GDCLASS(VariantList, Object);
-    
+	GDCLASS(VariantList, Object);
+
 protected:
-    static void _bind_methods();
+	static void _bind_methods();
 
 private:
 	VariantListData *_data = nullptr;
@@ -80,12 +80,11 @@ public:
 	void move_before(VariantListElement *value, VariantListElement *where);
 
 	void sort() {
-		sort_custom<Comparator<Variant> >();
+		sort_custom<Comparator<Variant>>();
 	}
 
 	template <class C>
 	void sort_custom_inplace() {
-
 		if (size() < 2)
 			return;
 
@@ -94,18 +93,15 @@ public:
 		VariantListElement *to = from;
 
 		while (current) {
-
 			VariantListElement *next = current->next_ptr;
 
 			if (from != current) {
-
 				current->prev_ptr = nullptr;
 				current->next_ptr = from;
 
 				VariantListElement *find = from;
 				C less;
 				while (find && less(find->value, current->value)) {
-
 					current->prev_ptr = find;
 					current->next_ptr = find->next_ptr;
 					find = find->next_ptr;
@@ -121,7 +117,6 @@ public:
 				else
 					to = current;
 			} else {
-
 				current->prev_ptr = nullptr;
 				current->next_ptr = nullptr;
 			}
@@ -134,10 +129,8 @@ public:
 
 	template <class C>
 	struct AuxiliaryComparator {
-
 		C compare;
 		bool operator()(const VariantListElement *a, const VariantListElement *b) const {
-
 			return compare(a->value, b->value);
 		}
 	};
@@ -152,12 +145,11 @@ public:
 
 		int idx = 0;
 		for (VariantListElement *E = front(); E; E = E->next_ptr) {
-
 			aux_buffer[idx] = E;
 			idx++;
 		}
 
-		SortArray<VariantListElement *, AuxiliaryComparator<C> > sort;
+		SortArray<VariantListElement *, AuxiliaryComparator<C>> sort;
 		sort.sort(aux_buffer, s);
 
 		_data->first = aux_buffer[0];
@@ -169,7 +161,6 @@ public:
 		aux_buffer[s - 1]->next_ptr = nullptr;
 
 		for (int i = 1; i < s - 1; i++) {
-
 			aux_buffer[i]->prev_ptr = aux_buffer[i - 1];
 			aux_buffer[i]->next_ptr = aux_buffer[i + 1];
 		}
@@ -178,6 +169,13 @@ public:
 	}
 	// VariantList(const VariantList &p_list);
 	VariantList() {}
+};
+
+template <>
+struct VariantCaster<VariantListElement*> {
+	static _FORCE_INLINE_ VariantListElement* cast(const Variant &p_variant) {
+		return (VariantListElement*)p_variant.operator Object*();
+	}
 };
 
 #endif // GOOST_LIST_H
--- tests/project/goost/core/test_list.gd
+++ tests/project/goost/core/test_list.gd
@@ -1,7 +1,10 @@
 extends "res://addons/gut/test.gd"
 
-func test_push_back():
+
+func test_push_pop_back():
 	var list = VariantList.new()
+
+	# Push back
 	var n: VariantListElement
 	n = list.push_back("Goost")
 	assert_eq(n.value, "Goost")
@@ -9,3 +12,18 @@ func test_push_back():
 	assert_eq(n.value, 37)
 	n = list.push_back(Color.red)
 	assert_eq(n.value, Color.red)
+
+	# Pop back
+	var v = null
+	v = list.back().value
+	list.pop_back()
+	assert_eq(v, Color.red)
+	v = list.back().value
+	list.pop_back()
+	assert_eq(v, 37)
+	v = list.back().value
+	list.pop_back()
+	assert_eq(v, "Goost")
+
+	assert_null(list.back())
+	assert_null(list.front())
