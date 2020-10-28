0b8968bdffc5523a42aa5a47a214e7a60c2cd762
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2020-10-28 22:47:28 +0200

    Do not provide default value for `GoostGeometry2D.regular_polygon`
    The `size` parameter should be a required one.

--- core/math/2d/geometry/goost_geometry_2d_bind.cpp
+++ core/math/2d/geometry/goost_geometry_2d_bind.cpp
@@ -414,7 +414,7 @@ void _GoostGeometry2D::_bind_methods() {
 
 	ClassDB::bind_method(D_METHOD("point_in_polygon", "point", "polygon"), &_GoostGeometry2D::point_in_polygon);
 
-	ClassDB::bind_method(D_METHOD("regular_polygon", "sides", "size"), &_GoostGeometry2D::regular_polygon, DEFVAL(64.0));
+	ClassDB::bind_method(D_METHOD("regular_polygon", "sides", "size"), &_GoostGeometry2D::regular_polygon);
 	ClassDB::bind_method(D_METHOD("circle", "radius", "max_error"), &_GoostGeometry2D::circle, DEFVAL(0.25));
 
 	ClassDB::bind_method(D_METHOD("rand_point_in_circle", "radius"), &_GoostGeometry2D::rand_point_in_circle);
