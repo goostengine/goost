59b7855128777b60af08ae0d5620f91985694809
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2020-11-03 01:05:37 +0200

    Add `choice` and `color_rgb` methods to `Random`

--- core/math/random.cpp
+++ core/math/random.cpp
@@ -55,12 +55,46 @@ Color Random::color_hsv(real_t h_min, real_t h_max, real_t s_min, real_t s_max, 
 	return Color(h, s, v, a);
 }
 
+Color Random::color_rgb(real_t r_min, real_t r_max, real_t g_min, real_t g_max, real_t b_min, real_t b_max, real_t a_min, real_t a_max) {
+	real_t r = randf_range(r_min, r_max);
+	real_t g = randf_range(g_min, g_max);
+	real_t b = randf_range(b_min, b_max);
+	real_t a = randf_range(a_min, a_max);
+	return Color(r, g, b, a);
+}
+
 Vector2 Random::circle_point(real_t p_radius_min, real_t p_radius_max) {
 	real_t r2_max = p_radius_max * p_radius_max;
 	real_t r2_min = p_radius_min * p_radius_min;
 	real_t r = Math::sqrt(randf_range(0.0, 1.0) * (r2_max - r2_min) + r2_min);
 	real_t t = randf_range(0.0, 1.0) * Math_TAU;
 	return Vector2(r * Math::cos(t), r * Math::sin(t));
+}
+
+Variant Random::choice(const Variant &p_sequence) {
+	switch (p_sequence.get_type()) {
+		case Variant::STRING: {
+			String str = p_sequence;
+			ERR_FAIL_COND_V_MSG(str.empty(), Variant(), "String is empty.");
+			return str.substr(randi() % str.length(), 1); // Not size().
+		} break;
+		case Variant::POOL_BYTE_ARRAY:
+		case Variant::POOL_INT_ARRAY:
+		case Variant::POOL_REAL_ARRAY:
+		case Variant::POOL_STRING_ARRAY:
+		case Variant::POOL_VECTOR2_ARRAY:
+		case Variant::POOL_VECTOR3_ARRAY:
+		case Variant::POOL_COLOR_ARRAY:
+		case Variant::ARRAY: {
+			Array arr = p_sequence;
+			ERR_FAIL_COND_V_MSG(arr.empty(), Variant(), "Array is empty.");
+			return arr[randi() % arr.size()];
+		} break;
+		default: {
+			ERR_FAIL_V_MSG(Variant(), "Unsupported type: the sequence must be indexable.")
+		}
+	}
+	return Variant();
 }
 
 void Random::_bind_methods() {
@@ -72,10 +106,15 @@ void Random::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("get_condition"), &Random::get_condition);
 
 	ClassDB::bind_method(D_METHOD("range", "from", "to"), &Random::range);
+
 	ClassDB::bind_method(D_METHOD("color_hsv", "hue_min", "hue_max", "saturation_min", "saturation_max", "value_min", "value_max", "alpha_min", "alpha_max"),
+			&Random::color_hsv, DEFVAL(0.0), DEFVAL(1.0), DEFVAL(0.0), DEFVAL(1.0), DEFVAL(0.0), DEFVAL(1.0), DEFVAL(1.0), DEFVAL(1.0));
+	ClassDB::bind_method(D_METHOD("color_rgb", "red_min", "red_max", "green_min", "green_max", "blue_min", "blue_max", "alpha_min", "alpha_max"),
 			&Random::color_hsv, DEFVAL(0.0), DEFVAL(1.0), DEFVAL(0.0), DEFVAL(1.0), DEFVAL(0.0), DEFVAL(1.0), DEFVAL(1.0), DEFVAL(1.0));
 
 	ClassDB::bind_method(D_METHOD("circle_point", "radius_min", "radius_max"), &Random::circle_point, DEFVAL(1.0), DEFVAL(1.0));
+
+	ClassDB::bind_method(D_METHOD("choice", "from_sequence"), &Random::choice);
 
 	ADD_PROPERTY(PropertyInfo(Variant::INT, "number"), "", "get_number");
 	ADD_PROPERTY(PropertyInfo(Variant::REAL, "value"), "", "get_value");
