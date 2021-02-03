96469df20cb6f01308be57f05b5bbdb837017e1e
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2021-02-03 20:45:58 +0200

    Add `invoke` method

--- core/goost_engine.cpp
+++ core/goost_engine.cpp
@@ -1,7 +1,7 @@
 #include "goost_engine.h"
 
-#include "core/engine.h"
 #include "core/color_names.inc"
+#include "core/os/os.h"
 
 GoostEngine *GoostEngine::singleton = nullptr;
 
@@ -47,6 +47,37 @@ Variant GoostEngine::_defer_call_unique_bind(const Variant **p_args, int p_argco
 	return Variant();
 }
 
+void GoostEngine::_invoke(Object *p_obj, StringName p_method, real_t p_sec, bool p_pause_mode, bool p_deferred) {
+	ERR_FAIL_NULL_MSG(p_obj, "Invalid object");
+
+	SceneTree *tree = Object::cast_to<SceneTree>(OS::get_singleton()->get_main_loop());
+	ERR_FAIL_NULL_MSG(tree, "The `invoke()` method requires a SceneTree to work.");
+
+	Ref<SceneTreeTimer> timer = tree->create_timer(p_sec, p_pause_mode);
+	timer->connect("timeout", this, "_on_invoke_timeout",
+			varray(p_obj->get_instance_id(), p_method), p_deferred ? CONNECT_DEFERRED : 0);
+}
+
+void GoostEngine::invoke(Object *p_obj, StringName p_method, real_t p_sec, bool p_pause_mode) {
+	_invoke(p_obj, p_method, p_sec, p_pause_mode, false);
+}
+
+void GoostEngine::invoke_deferred(Object *p_obj, StringName p_method, real_t p_sec, bool p_pause_mode) {
+	_invoke(p_obj, p_method, p_sec, p_pause_mode, true);
+}
+
+void GoostEngine::_on_invoke_timeout(ObjectID p_id, StringName p_method) {
+	Object *obj = ObjectDB::get_instance(p_id);
+	if (!obj) {
+		return; // Might be gone by this time.
+	}
+	Variant::CallError ce;
+	obj->call(p_method, nullptr, 0, ce);
+	if (ce.error != Variant::CallError::CALL_OK) {
+		ERR_PRINT("Error invoking method: " + Variant::get_call_error_text(obj, p_method, nullptr, 0, ce) + ".");
+	}
+}
+
 void GoostEngine::flush_calls() {
 	singleton->deferred_calls.flush();
 }
@@ -60,4 +91,7 @@ void GoostEngine::_bind_methods() {
 		mi.arguments.push_back(PropertyInfo(Variant::STRING, "method"));
 		ClassDB::bind_vararg_method(METHOD_FLAGS_DEFAULT, "defer_call_unique", &GoostEngine::_defer_call_unique_bind, mi, varray(), false);
 	}
+	ClassDB::bind_method(D_METHOD("invoke", "object", "method", "delay_seconds", "pause_mode_process"), &GoostEngine::invoke, DEFVAL(true));
+	ClassDB::bind_method(D_METHOD("invoke_deferred", "object", "method", "delay_seconds", "pause_mode_process"), &GoostEngine::invoke_deferred, DEFVAL(true));
+	ClassDB::bind_method(D_METHOD("_on_invoke_timeout"), &GoostEngine::_on_invoke_timeout);
 }
--- core/goost_engine.h
+++ core/goost_engine.h
@@ -1,6 +1,7 @@
 #ifndef GOOST_ENGINE_H
 #define GOOST_ENGINE_H
 
+#include "scene/main/scene_tree.h"
 #include "core/object.h"
 #include "func_buffer.h"
 
@@ -14,13 +15,20 @@ private:
 protected:
 	static void _bind_methods();
 
+	Variant _defer_call_unique_bind(const Variant **p_args, int p_argcount, Variant::CallError &r_error);
+
+	void _invoke(Object *p_obj, StringName p_method, real_t p_delay_seconds, bool p_pause_mode_process, bool p_deferred);
+	void _on_invoke_timeout(ObjectID p_id, StringName p_method);
+
 public:
 	static GoostEngine *get_singleton() { return singleton; }
 
 	Dictionary get_color_constants() const;
 
 	void defer_call_unique(Object *p_obj, StringName p_method, VARIANT_ARG_LIST);
-	Variant _defer_call_unique_bind(const Variant **p_args, int p_argcount, Variant::CallError &r_error);
+
+	void invoke(Object *p_obj, StringName p_method, real_t p_delay_seconds, bool p_pause_mode_process = true);
+	void invoke_deferred(Object *p_obj, StringName p_method, real_t p_delay_seconds, bool p_pause_mode_process = true);
 
 	static void flush_calls();
 
