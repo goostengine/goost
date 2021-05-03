67844dc6b4227a73cd1b2529be70c843b5b8b637
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2021-05-03 23:15:41 +0300

    Organize `run.py` tools argument parsing via subparsers
    Also added `--windowed` switch.

--- run.py
+++ run.py
@@ -46,14 +46,25 @@ if __name__ == "__main__":
         sys.exit(255)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("tool", help='The type of a tool to run: "editor", "tests", "doctool".')
+    parser.add_argument("--windowed", action="store_true", default=False,
+            help="Run the tool in windowed mode, disabled by default (Windows only).")
 
-    # Unit test arguments.
-    parser.add_argument("-t", "--test-file",
+    subparsers = parser.add_subparsers(dest="tool")
+
+    # Editor.
+    editor = subparsers.add_parser("editor", help="Run Godot editor.")
+    
+    # Unit tests.
+    tests = subparsers.add_parser("tests", help="Run Goost unit tests (run with `tests -h` for more options).")
+    tests.add_argument("-t", "--test-file",
             help='A relative path to test file to run, for instance: "core/math/test_random.gd"')
-    parser.add_argument("-tc", "--test-case",
+    tests.add_argument("-tc", "--test-case",
             help="Name of a test case to run. Any test case matching the name will be run.")
 
+    # Documentation.
+    doc = subparsers.add_parser("doc", help="Generate documentation.")
+
+    # Parse!
     args = parser.parse_args()
 
     if args.tool.startswith("editor"):
@@ -70,14 +81,18 @@ if __name__ == "__main__":
             res_file = "res://goost/" + args.test_file
             test_args.extend(["-gtest=%s" % res_file, '-gdir='])
         if args.test_case:
-            test_args.extend(["-gunit_test_name=%s" % args.test_case])
-        ret = run(test_args)
+            test_args.append("-gunit_test_name=%s" % args.test_case)
+        if not args.windowed:
+            # Not exiting on failure only makes sense while running in
+            # windowed mode, this does not matter for console output.
+            test_args.append("-gexit=true")
+        ret = run(test_args, windowed=args.windowed)
         sys.exit(ret)
     elif args.tool.startswith("doc"):
         print("Generating documentation ...")
         if not os.path.exists("doc/godot"):
             os.makedirs("doc/godot")
-        ret = run([godot_bin, "--doctool", os.path.join(base_path, "doc/godot")])
+        ret = run([godot_bin, "--doctool", os.path.join(base_path, "doc/godot")], windowed=args.windowed)
         sys.exit(ret)
     else:
         print('Error: tool not found. Run with `--help` to list available tools.')
