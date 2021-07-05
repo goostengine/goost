019313c6442a8c68cb16dd9f4eb4248bd79ac21a
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2021-07-05 18:33:21 +0300

    Use `config.py` to enable or disable GIF module
    The previous way of disabling the module was a bit hacky. Since GIF module could be compiled independently of Goost, we still need a way to automatically disable the module if it's built as part of Goost where `goost_image_enabled=no` might be specified.
    
    We rely on implicit module detection order. Since Goost is detected first, it's configuration is copied to base env in Godot, so other nested modules can refer to those options. If an option is not present, it means that the module is built independently of Goost.

--- modules/gif/config.py
+++ modules/gif/config.py
@@ -1,4 +1,8 @@
 def can_build(env, platform):
+    # This module is part of the Goost project, but can be built independently.
+    # Refer to https://github.com/goostengine/goost for more information.
+    if "goost_image_enabled" in env:
+        return env["goost_image_enabled"]
     return True
 
 
