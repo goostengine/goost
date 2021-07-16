2492e03adbf868fe7a673685a0a7d9dec9773300
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2021-07-16 17:42:46 +0300

    Fix unfreed `GridRect` in tests

--- tests/project/goost/scene/gui/test_grid_rect.gd
+++ tests/project/goost/scene/gui/test_grid_rect.gd
@@ -3,23 +3,18 @@ extends "res://addons/gut/test.gd"
 
 func test_snap():
 	var grid = preload("grid_rect.tscn").instance()
-	add_child(grid)
+	add_child_autofree(grid)
 	watch_signals(grid)
 	yield(grid, "finished")
 	assert_signal_emitted(grid, "finished")
-	grid.queue_free()
-	grid = null
 
 
 func test_metadata():
 	var grid = GridRect.new()
-	add_child(grid)
+	add_child_autofree(grid)
 
 	grid.set_cell_metadata(Vector2(-1, 1), Color.blue)
 	assert_eq(grid.get_cell_metadata(Vector2(-1, 1)), Color.blue)
 
 	grid.clear_cell_metadata()
 	assert_eq(grid.get_cell_metadata(Vector2(-1, 1)), null)
-
-	grid.queue_free()
-	grid = null
