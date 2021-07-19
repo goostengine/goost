b3f0aef495bad72e7fb19ff4d2c435ae3df3f55c
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2021-07-20 01:01:27 +0300

    Fix `ImageFrames.load_gif_from_buffer()` crash with invalid data

---
+++ tests/project/goost/core/image/test_image_frames.gd
@@ -0,0 +1,8 @@
+extends "res://addons/gut/test.gd"
+
+# TODO: enable once error printing can be disabled.
+# func test_image_frames_invalid_data():
+# 	var frames = ImageFrames.new()
+# 	frames.get_frame_image(-20)
+# 	frames.load(".", 4)
+# 	frames.load_gif_from_buffer(PoolByteArray([]), -31)
