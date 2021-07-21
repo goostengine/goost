88815c714e9f9bd7041a52a2deaef6527536b0ae
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2021-07-21 16:10:36 +0300

    Do not build `libgit2` on Linux server platform
    It's only useful for editor builds, not headless builds (cause build errors anyway).

--- modules/git/config.py
+++ modules/git/config.py
@@ -1,15 +1,18 @@
 import os
 
+# This module is part of the Goost project, but can be built independently.
+# Refer to https://github.com/goostengine/goost for more information.
 def can_build(env, platform):
     libgit2_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "thirdparty/libgit2/src")
     if not os.path.exists(libgit2_path):
         return False
 
-    # This module is part of the Goost project, but can be built independently.
-    # Refer to https://github.com/goostengine/goost for more information.
+    build = env["tools"] and env["platform"] != "server"
+
     if "goost_vcs_enabled" in env:
-        return env["goost_vcs_enabled"] and env["tools"]
-    return env["tools"]
+        return build and env["goost_vcs_enabled"]
+
+    return build
 
 def configure(env):
 	pass
