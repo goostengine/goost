46379f65c5bc9324dc0be1cb9c13f34c22b681b2
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2021-08-05 17:28:38 +0300

    Prevent setting too big or too small collision mask in `ShapeCast2D`

--- scene/physics/2d/shape_cast_2d.cpp
+++ scene/physics/2d/shape_cast_2d.cpp
@@ -43,15 +43,19 @@ uint32_t ShapeCast2D::get_collision_mask() const {
 }
 
 void ShapeCast2D::set_collision_mask_bit(int p_bit, bool p_value) {
+	ERR_FAIL_INDEX_MSG(p_bit, 32, "Collision mask bit must be between 0 and 31 inclusive.");
+
 	uint32_t mask = get_collision_mask();
-	if (p_value)
+	if (p_value) {
 		mask |= 1 << p_bit;
-	else
+	} else {
 		mask &= ~(1 << p_bit);
+	}
 	set_collision_mask(mask);
 }
 
 bool ShapeCast2D::get_collision_mask_bit(int p_bit) const {
+	ERR_FAIL_INDEX_V_MSG(p_bit, 32, false, "Collision mask bit must be between 0 and 31 inclusive.");
 	return get_collision_mask() & (1 << p_bit);
 }
 
