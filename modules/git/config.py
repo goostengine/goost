3fbffd01b77b6db8deb85101edba1c6f873bfb48
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2021-11-06 13:24:40 +0200

    More robust way of detecting libgit2 submodule

--- modules/git/config.py
+++ modules/git/config.py
@@ -1,10 +1,15 @@
 import os
 
-# This module is part of the Goost project, but can be built independently.
+# This module is maintained as part of Goost, but can be built independently.
 # Refer to https://github.com/goostengine/goost for more information.
+
 def can_build(env, platform):
-    libgit2_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "thirdparty/libgit2/src")
-    if not os.path.exists(libgit2_path):
+    libgit2_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "thirdparty/libgit2/")
+
+    if os.path.isdir(libgit2_path):
+        if not os.listdir(libgit2_path):
+            return False
+    else:
         return False
 
     build = env["tools"] and env["platform"] != "server"
--- modules/git/thirdparty/README.md
+++ modules/git/thirdparty/README.md
@@ -4,5 +4,5 @@ The following lists C/C++ libraries which are used by git module.
 
 ## libgit2
 - Upstream: https://github.com/libgit2/libgit2
-- Version: v1.1.1
+- Version: v1.3.0
 - License: GPLv2-only with linking exception
