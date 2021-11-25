040925b1ed5a83fd178449a59c7ea866dd75d6a5
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2021-11-25 19:03:53 +0200

    Add `GoostMath` singleton
    This will be the new home for math functions in Goost.
    
    Implemented Bezier and Catmull-Rom interpolation functions.
    
    Added `is_equal_approx()` and `is_zero_approx()` with configurable tolerance (which is impossible to configure in Godot's `is_equal_approx()`).

---
+++ core/math/math.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "core/object.h"
+
+#define GOOST_CMP_EPSILON 0.000001
+
+class GoostMath : public Object {
+	GDCLASS(GoostMath, Object);
+
+private:
+	static GoostMath *singleton;
+
+protected:
+	static void _bind_methods();
+
+public:
+	static GoostMath *get_singleton() { return singleton; }
+
+    bool is_equal_approx(real_t a, real_t b, real_t tolerance = GOOST_CMP_EPSILON);
+    bool is_zero_approx(real_t s, real_t tolerance = GOOST_CMP_EPSILON);
+
+	Variant catmull_rom(const Variant &p0, const Variant &p1, const Variant &p2, const Variant &p3, float t);
+	Variant bezier(const Variant &p0, const Variant &p1, const Variant &p2, const Variant &p3, float t);
+
+	GoostMath() {
+		ERR_FAIL_COND_MSG(singleton != nullptr, "Singleton already exists");
+		singleton = this;
+	}
+};
+
+namespace goost {
+
+template <class T>
+T catmull_rom(const T &p0, const T &p1, const T &p2, const T &p3, float t) {
+	float t2 = t * t;
+	float t3 = t2 * t;
+
+	return 0.5f * ((2.0f * p1) + (-p0 + p2) * t + (2.0f * p0 - 5.0f * p1 + 4 * p2 - p3) * t2 + (-p0 + 3.0f * p1 - 3.0f * p2 + p3) * t3);
+}
+
+template <class T>
+T bezier(T start, T control_1, T control_2, T end, float t) {
+	real_t it = (1.0 - t);
+	real_t it2 = it * it;
+	real_t it3 = it2 * it;
+	real_t t2 = t * t;
+	real_t t3 = t2 * t;
+
+	return start * it3 + control_1 * it2 * t * 3.0 + control_2 * it * t2 * 3.0 + end * t3;
+}
+
+} // namespace goost
