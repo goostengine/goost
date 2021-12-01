587016418b0ad9333507da8f7af4195260ffb1ac
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2021-12-01 19:26:10 +0200

    Codestyle, doc, and consistency fixes for `MidiPlayer`

--- scene/audio/midi_player.cpp
+++ scene/audio/midi_player.cpp
@@ -1,5 +1,5 @@
-/* 
-Copyright (c) 2021 Filip Anton (filipworks) 
+/*
+Copyright (c) 2021 Filip Anton (filipworks)
 Created for Goblin Engine github.com/goblinengine
 Initial implementation based on https://github.com/RodZill4/godot-music
 
@@ -24,20 +24,19 @@ SOFTWARE.
 
 #include "midi_player.h"
 
-#include "core/bind/core_bind.h"
 #define TSF_IMPLEMENTATION
-#include "../thirdparty/tsf/tsf.h"
+#include "thirdparty/tsf/tsf.h"
 #define TML_IMPLEMENTATION
-#include "../thirdparty/tsf/tml.h"
+#include "thirdparty/tsf/tml.h"
 
-// MIDIFILE
 void MidiFile::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("set_data", "data"), &MidiFile::set_data);
 	ClassDB::bind_method(D_METHOD("get_data"), &MidiFile::get_data);
-	ADD_PROPERTY(PropertyInfo(Variant::POOL_BYTE_ARRAY, "data", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_NOEDITOR), "set_data", "get_data");
 
 	ClassDB::bind_method(D_METHOD("set_format", "format"), &MidiFile::set_format);
 	ClassDB::bind_method(D_METHOD("get_format"), &MidiFile::get_format);
+
+	ADD_PROPERTY(PropertyInfo(Variant::POOL_BYTE_ARRAY, "data", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_NOEDITOR), "set_data", "get_data");
 	ADD_PROPERTY(PropertyInfo(Variant::INT, "format"), "set_format", "get_format");
 
 	BIND_ENUM_CONSTANT(FORMAT_MIDI);
@@ -54,7 +53,6 @@ PoolByteArray MidiFile::get_data() const {
 			memcpy(w.ptr(), data, data_len);
 		}
 	}
-
 	return vdata;
 }
 
@@ -64,7 +62,6 @@ void MidiFile::set_data(const PoolVector<uint8_t> &dataIn) {
 	memcpy(data, dataIn.read().ptr(), data_len);
 }
 
-// MIDIFILE IMPORTER
 void ResourceImporterMidiFile::get_recognized_extensions(List<String> *p_extensions) const {
 	p_extensions->push_back("sf2");
 	p_extensions->push_back("midi");
@@ -74,7 +71,7 @@ void ResourceImporterMidiFile::get_recognized_extensions(List<String> *p_extensi
 Error ResourceImporterMidiFile::import(const String &p_source_file, const String &p_save_path, const Map<StringName, Variant> &p_options, List<String> *r_platform_variants, List<String> *r_gen_files, Variant *r_metadata) {
 	FileAccess *f = FileAccess::open(p_source_file, FileAccess::READ);
 	ERR_FAIL_COND_V(!f, ERR_FILE_CANT_OPEN);
-	
+
 	uint64_t len = f->get_len();
 
 	PoolVector<uint8_t> data;
@@ -91,106 +88,86 @@ Error ResourceImporterMidiFile::import(const String &p_source_file, const String
 	ERR_FAIL_COND_V(!mdf->get_data().size(), ERR_FILE_CORRUPT);
 
 	if (p_source_file.ends_with("sf2")) {
-		mdf->set_format(1);
+		mdf->set_format(MidiFile::FORMAT_SF2);
 	}
-
 	return ResourceSaver::save(p_save_path + ".mdf", mdf);
-
-	return OK;
 }
 
-// MIDIPLAYER
-MidiPlayer::MidiPlayer(): mTsf(NULL), midi_speed(1.0) {
-	mTsf = NULL;
-	//mSoundFontName = "";
-	mTml = NULL;
-	//mMidiName = "";
-	midiCurrent = NULL;
-	looping = true;
-}
-
-// convenience load + set
+// Convenience load + set.
 void MidiPlayer::load_soundfont(String inSoundFontName) {
 	Ref<MidiFile> sf = ResourceLoader::load(inSoundFontName);
-	ERR_FAIL_COND_MSG(sf == nullptr, "Couldn't open soundfont " + inSoundFontName + ".");
+	ERR_FAIL_COND_MSG(sf.is_null(), "Couldn't open soundfont " + inSoundFontName + ".");
 
 	set_soundfont(sf);
 }
 
 void MidiPlayer::set_soundfont(Ref<MidiFile> sf) {
 	soundfont = sf;
-	if (sf == nullptr) return;
-
-	if (sf->format < 1) {
-		print_error("Resource is not a SoundFont");
+	if (sf.is_null()) {
 		return;
 	}
-
-	// Clear the old sound font
-	if (mTsf != NULL) {
-		tsf_close(mTsf);
-		mTsf = NULL;
+	if (sf->format != MidiFile::FORMAT_SF2) {
+		ERR_PRINT("Resource is not a SoundFont");
+		return;
 	}
-	
-	if (soundfont != nullptr) {
-		//load data in memory
+	// Clear the old sound font.
+	if (mTsf != nullptr) {
+		tsf_close(mTsf);
+		mTsf = nullptr;
+	}
+	if (soundfont.is_valid()) {
+		// Load data in memory.
 		int data_len = soundfont->get_data().size();
 		PoolVector<uint8_t> dataIn = soundfont->get_data();
-		void * dataOut = memalloc(data_len);
+		void *dataOut = memalloc(data_len);
 		memcpy(dataOut, dataIn.read().ptr(), data_len);
-		//load tiny sound font using memory data
+		// Load tiny sound font using memory data.
 		mTsf = tsf_load_memory(dataOut, data_len);
 	}
 }
-
 
 Ref<MidiFile> MidiPlayer::get_soundfont() const {
 	return soundfont;
 }
 
-// convenience load + set
+// Convenience load + set.
 void MidiPlayer::load_midi(String inMidiFileName) {
-	// Open a new midi file
-	//midiFile = FileAccess::open(mMidiName, FileAccess::READ);
 	Ref<MidiFile> mid = ResourceLoader::load(inMidiFileName);
-	ERR_FAIL_COND_MSG(mid == nullptr, "Couldn't open " + inMidiFileName + ".");
+	ERR_FAIL_COND_MSG(mid.is_null(), "Couldn't open " + inMidiFileName + ".");
 
 	set_midi(mid);
 }
 
 void MidiPlayer::set_midi(Ref<MidiFile> mid) {
 	midi = mid;
-	if (midi == nullptr) return;
-	
-	if (midi->format != 0) {
-		print_error("Resource not a Midi file.");
+	if (midi.is_null()) {
 		return;
 	}
-
-	// Clear the current midi
-	if (mTml != NULL) {
-		tml_free(mTml);
-		mTml = NULL;
-		midiCurrent = NULL;
-		//midiFile->close();
+	if (midi->format != MidiFile::FORMAT_MIDI) {
+		ERR_PRINT("Resource not a MIDI file.");
+		return;
 	}
-
-	// Load the new midi and set parameters
-	if (midi != nullptr) {
-		//load data in memory
+	// Clear the current midi.
+	if (mTml != nullptr) {
+		tml_free(mTml);
+		mTml = nullptr;
+		midiCurrent = nullptr;
+	}
+	// Load the new midi and set parameters.
+	if (midi.is_valid()) {
+		// Load data in memory.
 		int data_len = midi->get_data().size();
 		PoolVector<uint8_t> dataIn = midi->get_data();
-		void * dataOut = memalloc(data_len);
+		void *dataOut = memalloc(data_len);
 		memcpy(dataOut, dataIn.read().ptr(), data_len);
-		//load tiny midy loader using memory data
+		// Load tiny midi loader using memory data.
 		mTml = tml_load_memory(dataOut, data_len);
 
-		//set midi to start at the beginning
+		// Set midi to start at the beginning.
 		midiCurrent = mTml;
 		midiTime = 0.0;
 	}
 }
-
 
 Ref<MidiFile> MidiPlayer::get_midi() const {
 	return midi;
@@ -199,7 +176,7 @@ Ref<MidiFile> MidiPlayer::get_midi() const {
 PoolStringArray MidiPlayer::get_preset_names() const {
 	PoolStringArray theReturnValue;
 
-	if (mTsf != NULL) {
+	if (mTsf != nullptr) {
 		unsigned theIndex, theEnd;
 
 		for (theIndex = 0, theEnd = tsf_get_presetcount(mTsf); theIndex < theEnd; ++theIndex) {
@@ -210,14 +187,14 @@ PoolStringArray MidiPlayer::get_preset_names() const {
 }
 
 int MidiPlayer::get_preset_index(int inBank, int inPresetNumber) {
-	if (mTsf == NULL) {
+	if (mTsf == nullptr) {
 		return -1;
 	}
 	return tsf_get_presetindex(mTsf, inBank, inPresetNumber);
 }
 
 void MidiPlayer::note_on(int inPresetIndex, int inKey, float inVelocity) {
-	if (mTsf == NULL) {
+	if (mTsf == nullptr) {
 		return;
 	}
 	if (inVelocity > 0.0) {
@@ -232,169 +209,161 @@ void MidiPlayer::note_off(int inPresetIndex, int inKey) {
 }
 
 void MidiPlayer::note_off_all() {
-	if (mTsf == NULL) {
+	if (mTsf == nullptr) {
 		return;
 	}
 	tsf_note_off_all(mTsf);
 }
 
 void MidiPlayer::channel_set_preset_index(int inChannel, int inPresetIndex) {
-	if (mTsf == NULL) {
+	if (mTsf == nullptr) {
 		return;
 	}
 	tsf_channel_set_presetindex(mTsf, inChannel, inPresetIndex);
 }
 
-
 int MidiPlayer::channel_set_preset_number(int inChannel, int inPresetNumber, int inDrums) {
-	if (mTsf == NULL) {
+	if (mTsf == nullptr) {
 		return -1;
 	}
 	return tsf_channel_set_presetnumber(mTsf, inChannel, inPresetNumber, inDrums);
 }
 
-
 void MidiPlayer::channel_set_bank(int inChannel, int inBank) {
-	if (mTsf == NULL) {
+	if (mTsf == nullptr) {
 		return;
 	}
 	tsf_channel_set_bank(mTsf, inChannel, inBank);
 }
 
-
 int MidiPlayer::channel_set_bank_preset(int inChannel, int inBank, int inPresetNumber) {
-	if (mTsf == NULL) {
+	if (mTsf == nullptr) {
 		return -1;
 	}
 	return tsf_channel_set_bank_preset(mTsf, inChannel, inBank, inPresetNumber);
 }
 
-
 void MidiPlayer::channel_set_pan(int inChannel, float inPan) {
-	if (mTsf == NULL) {
+	if (mTsf == nullptr) {
 		return;
 	}
 	tsf_channel_set_pan(mTsf, inChannel, inPan);
 }
 
 void MidiPlayer::channel_set_volume(int inChannel, float inVolume) {
-	if (mTsf == NULL) {
+	if (mTsf == nullptr) {
 		return;
 	}
 	tsf_channel_set_volume(mTsf, inChannel, inVolume);
 }
 
 void MidiPlayer::channel_set_pitchwheel(int inChannel, int inPitchWheel) {
-	if (mTsf == NULL) {
+	if (mTsf == nullptr) {
 		return;
 	}
 	tsf_channel_set_pitchwheel(mTsf, inChannel, inPitchWheel);
 }
 
 void MidiPlayer::channel_set_pitchrange(int inChannel, float inPitchRange) {
-	if (mTsf == NULL) {
+	if (mTsf == nullptr) {
 		return;
 	}
 	tsf_channel_set_pitchrange(mTsf, inChannel, inPitchRange);
 }
 
 void MidiPlayer::channel_set_tuning(int inChannel, float inTuning) {
-	if (mTsf == NULL) {
+	if (mTsf == nullptr) {
 		return;
 	}
 	tsf_channel_set_tuning(mTsf, inChannel, inTuning);
 }
 
 void MidiPlayer::channel_note_on(int inChannel, int inKey, float inVelocity) {
-	if (mTsf == NULL) {
+	if (mTsf == nullptr) {
 		return;
 	}
 	tsf_channel_note_on(mTsf, inChannel, inKey, inVelocity);
 }
 
 void MidiPlayer::channel_note_off(int inChannel, int inKey) {
-	if (mTsf == NULL) {
+	if (mTsf == nullptr) {
 		return;
 	}
 	tsf_channel_note_off(mTsf, inChannel, inKey);
 }
 
 void MidiPlayer::channel_note_off_all(int inChannel) {
-	if (mTsf == NULL) {
+	if (mTsf == nullptr) {
 		return;
 	}
 	tsf_channel_note_off_all(mTsf, inChannel);
 }
 
-// end with sustain and release
 void MidiPlayer::channel_sounds_off_all(int inChannel) {
-	if (mTsf == NULL) {
+	if (mTsf == nullptr) {
 		return;
 	}
 	tsf_channel_sounds_off_all(mTsf, inChannel);
 }
 
-// end immediatly
 void MidiPlayer::channel_midi_control(int inChannel, int inController, int inValue) {
-	if (mTsf == NULL) {
+	if (mTsf == nullptr) {
 		return;
 	}
 	tsf_channel_midi_control(mTsf, inChannel, inController, inValue);
 }
 
 int MidiPlayer::channel_get_preset_index(int inChannel) {
-	if (mTsf == NULL) {
+	if (mTsf == nullptr) {
 		return -1;
 	}
 	return tsf_channel_get_preset_index(mTsf, inChannel);
 }
 
 int MidiPlayer::channel_get_preset_bank(int inChannel) {
-	if (mTsf == NULL) {
+	if (mTsf == nullptr) {
 		return -1;
 	}
 	return tsf_channel_get_preset_bank(mTsf, inChannel);
 }
 
 int MidiPlayer::channel_get_preset_number(int inChannel) {
-	if (mTsf == NULL) {
+	if (mTsf == nullptr) {
 		return -1;
 	}
 	return tsf_channel_get_preset_number(mTsf, inChannel);
 }
 
-
 float MidiPlayer::channel_get_pan(int inChannel) {
-	if (mTsf == NULL) {
+	if (mTsf == nullptr) {
 		return 0.0;
 	}
 	return tsf_channel_get_pan(mTsf, inChannel);
 }
 
 float MidiPlayer::channel_get_volume(int inChannel) {
-	if (mTsf == NULL) {
+	if (mTsf == nullptr) {
 		return 0.0;
 	}
 	return tsf_channel_get_volume(mTsf, inChannel);
 }
 
-
 int MidiPlayer::channel_get_pitchwheel(int inChannel) {
-	if (mTsf == NULL) {
+	if (mTsf == nullptr) {
 		return 0;
 	}
 	return tsf_channel_get_pitchwheel(mTsf, inChannel);
 }
 
 float MidiPlayer::channel_get_pitchrange(int inChannel) {
-	if (mTsf == NULL) {
+	if (mTsf == nullptr) {
 		return 0.0;
 	}
 	return tsf_channel_get_pitchrange(mTsf, inChannel);
 }
 
 float MidiPlayer::channel_get_tuning(int inChannel) {
-	if (mTsf == NULL) {
+	if (mTsf == nullptr) {
 		return 0.0;
 	}
 	return tsf_channel_get_tuning(mTsf, inChannel);
@@ -402,86 +371,86 @@ float MidiPlayer::channel_get_tuning(int inChannel) {
 
 PoolVector2Array MidiPlayer::get_buffer(int inSize) {
 	PoolVector2Array theBuffer;
-	
-	if (inSize > 0 && mTsf != NULL) {
+
+	if (inSize > 0 && mTsf != nullptr) {
 		theBuffer.resize(inSize);
 		Vector2 *theData = theBuffer.write().ptr();
- 
-		// if there is a midi loaded play the midi
-		if (midiCurrent != NULL) {
+
+		// If there is a midi loaded play the midi.
+		if (midiCurrent != nullptr) {
 			int SampleBlock;
 			int SampleCount = inSize;
 			for (SampleBlock = TSF_RENDER_EFFECTSAMPLEBLOCK; SampleCount; SampleCount -= SampleBlock, theData += SampleBlock) {
-				// We progress the MIDI playback and then process TSF_RENDER_EFFECTSAMPLEBLOCK samples at once
-				if (SampleBlock > SampleCount) SampleBlock = SampleCount;
+				// We progress the MIDI playback and then process TSF_RENDER_EFFECTSAMPLEBLOCK samples at once.
+				if (SampleBlock > SampleCount)
+					SampleBlock = SampleCount;
 
-				// Loop through all MIDI messages which need to be played up until the current playback time
+				// Loop through all MIDI messages which need to be played up until the current playback time.
 				for (midiTime += SampleBlock * midi_speed * (1000.0 / 44100.0); midiCurrent && midiTime >= midiCurrent->time; midiCurrent = midiCurrent->next) {
 					switch (midiCurrent->type) {
-						case TML_PROGRAM_CHANGE: //channel program (preset) change (special handling for 10th MIDI channel with drums)
+						case TML_PROGRAM_CHANGE: // Channel program (preset) change (special handling for 10th MIDI channel with drums).
 							tsf_channel_set_presetnumber(mTsf, midiCurrent->channel, midiCurrent->program, (midiCurrent->channel == 9));
 							break;
-						case TML_NOTE_ON: //play a note
+						case TML_NOTE_ON: // Play a note.
 							tsf_channel_note_on(mTsf, midiCurrent->channel, midiCurrent->key, midiCurrent->velocity / 127.0f);
 							break;
-						case TML_NOTE_OFF: //stop a note
+						case TML_NOTE_OFF: // Stop a note.
 							tsf_channel_note_off(mTsf, midiCurrent->channel, midiCurrent->key);
 							break;
-						case TML_PITCH_BEND: //pitch wheel modification
+						case TML_PITCH_BEND: // Pitch wheel modification.
 							tsf_channel_set_pitchwheel(mTsf, midiCurrent->channel, midiCurrent->pitch_bend);
 							break;
-						case TML_CONTROL_CHANGE: //MIDI controller messages
+						case TML_CONTROL_CHANGE: // MIDI controller messages.
 							tsf_channel_midi_control(mTsf, midiCurrent->channel, midiCurrent->control, midiCurrent->control_value);
 							break;
 					}
 				}
-				// Render the block of audio samples in float format
-				// play the soundfont samples from note_on fuctions and from midi file
-				tsf_render_float(mTsf, (float*)theData, SampleBlock, 0);
+				// Render the block of audio samples in float format.
+				// play the soundfont samples from note_on fuctions and from midi file.
+				tsf_render_float(mTsf, (float *)theData, SampleBlock, 0);
 			}
 		} else {
-			// otherwise if there is a midi loaded but finished then loop if looping or else stop
+			// Otherwise if there is a midi loaded but finished then loop if looping or else stop.
 			if (mTml) {
 				if (looping == true) {
 					note_off_all();
 					midiCurrent = mTml;
-					midiTime = 0.0;	
-					// If looping, emit loop_finished instead of finished
+					midiTime = 0.0;
+					// If looping, emit loop_finished instead of finished.
 					emit_signal("loop_finished");
 				} else {
 					stop();
-					// stop() automatically emits finished but this will likely change in the future
+					// stop() automatically emits finished but this will likely change in the future,
 					// see https://github.com/godotengine/godot/pull/33602
 					// commmenting this out for now until above change is merged
 					// emit_signal("finished");
-				}	
+				}
 			}
-
-			// play the soundfont samples even without midi loaded
-			tsf_render_float(mTsf, (float*)theData, inSize, 0);
+			// Play the soundfont samples even without midi loaded.
+			tsf_render_float(mTsf, (float *)theData, inSize, 0);
 		}
 	}
 	return theBuffer;
 }
 
-// handle ready and interan process which feeds the buffer
+// Handle ready and internal process which feeds the buffer.
 void MidiPlayer::_notification(int p_what) {
- 	switch (p_what) {
+	switch (p_what) {
 		case NOTIFICATION_READY: {
 			set_process_internal(true);
 		}
 		case NOTIFICATION_INTERNAL_PROCESS: {
-			// if soundfont and stream exist then start buferring
+			// If soundfont and stream exist then start buferring.
 			if (mTsf) {
 				playback_gen = get_stream_playback();
 				if (playback_gen != nullptr) {
 					playback_gen->push_buffer(get_buffer(playback_gen->get_frames_available()));
-				} 
-			} 
+				}
+			}
 			break;
 		}
 	}
-} 
+}
 
 void MidiPlayer::set_looping(bool p_looping) {
 	looping = p_looping;
@@ -503,12 +472,10 @@ void MidiPlayer::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("load_soundfont", "soundfont_resource_path"), &MidiPlayer::load_soundfont);
 	ClassDB::bind_method(D_METHOD("set_soundfont", "soundfont_resource"), &MidiPlayer::set_soundfont);
 	ClassDB::bind_method(D_METHOD("get_soundfont"), &MidiPlayer::get_soundfont);
-	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "soundfont", PROPERTY_HINT_RESOURCE_TYPE, "MidiFile"), "set_soundfont", "get_soundfont");
 
 	ClassDB::bind_method(D_METHOD("load_midi", "midi_resource_path"), &MidiPlayer::load_midi);
 	ClassDB::bind_method(D_METHOD("set_midi", "midi_resource"), &MidiPlayer::set_midi);
 	ClassDB::bind_method(D_METHOD("get_midi"), &MidiPlayer::get_midi);
-	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "midi", PROPERTY_HINT_RESOURCE_TYPE, "MidiFile"), "set_midi", "get_midi");
 
 	ClassDB::bind_method(D_METHOD("get_preset_names"), &MidiPlayer::get_preset_names);
 
@@ -516,7 +483,6 @@ void MidiPlayer::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("note_off", "preset", "note"), &MidiPlayer::note_off);
 	ClassDB::bind_method(D_METHOD("note_off_all"), &MidiPlayer::note_off_all);
 
-	// change everything to D_METHOD(func, params)
 	ClassDB::bind_method(D_METHOD("channel_set_preset_index", "channel", "preset_index"), &MidiPlayer::channel_set_preset_index);
 	ClassDB::bind_method(D_METHOD("channel_set_preset_number", "channel", "preset_number", "drums"), &MidiPlayer::channel_set_preset_number);
 	ClassDB::bind_method(D_METHOD("channel_set_bank", "channel", "bank"), &MidiPlayer::channel_set_bank);
@@ -541,17 +507,21 @@ void MidiPlayer::_bind_methods() {
 
 	ClassDB::bind_method(D_METHOD("set_looping", "looping"), &MidiPlayer::set_looping);
 	ClassDB::bind_method(D_METHOD("get_looping"), &MidiPlayer::get_looping);
-	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "looping", PROPERTY_HINT_NONE, "", true), "set_looping", "get_looping");
+
 	ClassDB::bind_method(D_METHOD("set_midi_speed", "speed"), &MidiPlayer::set_midi_speed);
 	ClassDB::bind_method(D_METHOD("get_midi_speed"), &MidiPlayer::get_midi_speed);
+
+	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "soundfont", PROPERTY_HINT_RESOURCE_TYPE, "MidiFile"), "set_soundfont", "get_soundfont");
+	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "midi", PROPERTY_HINT_RESOURCE_TYPE, "MidiFile"), "set_midi", "get_midi");
+
+	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "looping", PROPERTY_HINT_NONE, "", true), "set_looping", "get_looping");
 	ADD_PROPERTY(PropertyInfo(Variant::REAL, "midi_speed", PROPERTY_HINT_RANGE, "0,8,0.1", 1.0), "set_midi_speed", "get_midi_speed");
 
 	ADD_SIGNAL(MethodInfo("loop_finished"));
 }
 
 MidiPlayer::~MidiPlayer() {
-	// frees memory related to soundfont
-	if (mTsf != NULL) {
+	if (mTsf != nullptr) {
 		tsf_close(mTsf);
 	}
 }
--- scene/audio/midi_player.h
+++ scene/audio/midi_player.h
@@ -1,5 +1,5 @@
-/* 
-Copyright (c) 2021 Filip Anton (filipworks) 
+/*
+Copyright (c) 2021 Filip Anton (filipworks)
 Created for Goblin Engine github.com/goblinengine
 Initial implementation based on https://github.com/RodZill4/godot-music
 
@@ -22,18 +22,18 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-#ifndef SoundFont_H
-#define SoundFont_H
+#pragma once
+
+#include "thirdparty/tsf/tml.h"
+#include "thirdparty/tsf/tsf.h"
+
 #include "core/io/resource_importer.h"
-#include "servers/audio/effects/audio_stream_generator.h"
-#include "scene/main/node.h"
 #include "core/os/file_access.h"
-#include "scene/audio/audio_stream_player.h"
-#include "../thirdparty/tsf/tsf.h"
-#include "../thirdparty/tsf/tml.h"
 #include "core/resource.h"
 
-// MIDIFILE RESOURCE
+#include "scene/audio/audio_stream_player.h"
+#include "servers/audio/effects/audio_stream_generator.h"
+
 class MidiFile : public Resource {
 	GDCLASS(MidiFile, Resource);
 	OBJ_CATEGORY("Resources");
@@ -46,28 +46,21 @@ protected:
 	static void _bind_methods();
 
 public:
-	enum Format { //this format stuff doesn't work yet (will come back to it later)
-		FORMAT_MIDI = 0,
+	enum Format {
+		FORMAT_MIDI,
 		FORMAT_SF2,
 	};
+	int format = FORMAT_MIDI; // Default.
 
-	// set default format
-	int format = FORMAT_MIDI; 
-
-	void set_format(int f) { format = f; }
+	void set_format(Format p_format) { format = p_format; }
 	int get_format() const { return format; }
 
 	void set_data(const PoolVector<uint8_t> &p_data);
 	PoolVector<uint8_t> get_data() const;
-
-	MidiFile() {};
-	~MidiFile() {};
 };
 
-MAKE_ENUM_TYPE_INFO(MidiFile::Format);
+VARIANT_ENUM_CAST(MidiFile::Format);
 
-
-// MIDIFILE IMPORTER
 class ResourceImporterMidiFile : public ResourceImporter {
 	GDCLASS(ResourceImporterMidiFile, ResourceImporter);
 
@@ -81,28 +74,25 @@ public:
 	virtual int get_preset_count() const { return 0; }
 	virtual String get_preset_name(int p_idx) const { return String(); }
 
-	virtual void get_import_options(List<ImportOption> *r_options, int p_preset = 0) const { }
+	virtual void get_import_options(List<ImportOption> *r_options, int p_preset = 0) const {}
 	virtual bool get_option_visibility(const String &p_option, const Map<StringName, Variant> &p_options) const { return true; }
 
 	virtual Error import(const String &p_source_file, const String &p_save_path, const Map<StringName, Variant> &p_options, List<String> *r_platform_variants, List<String> *r_gen_files = nullptr, Variant *r_metadata = nullptr);
-
-	ResourceImporterMidiFile() {};
 };
 
-// MIDI PLAYER
 class MidiPlayer : public AudioStreamPlayer {
 	GDCLASS(MidiPlayer, AudioStreamPlayer);
 
 private:
 	Ref<AudioStreamGeneratorPlayback> playback_gen;
 	Ref<MidiFile> soundfont;
-	tsf         *mTsf;
+	tsf *mTsf = nullptr;
 	Ref<MidiFile> midi;
-	tml_message *mTml;
-	tml_message *midiCurrent;
-	double       midiTime;
-	float        midi_speed;
-	bool         looping;
+	tml_message *mTml = nullptr;
+	tml_message *midiCurrent = nullptr;
+	double midiTime;
+	float midi_speed = 1.0;
+	bool looping = true;
 
 protected:
 	static void _bind_methods();
@@ -110,9 +100,6 @@ protected:
 	PoolVector2Array get_buffer(int inSize);
 
 public:
-	MidiPlayer();
-	~MidiPlayer();
-
 	void load_soundfont(String inSoundFontName);
 	void set_soundfont(Ref<MidiFile> sf);
 	Ref<MidiFile> get_soundfont() const;
@@ -130,33 +117,33 @@ public:
 	int get_preset_index(int inBank, int inPresetNumber);
 
 	void set_output(int inSampleRate, float inGainDb);
-	
+
 	void note_on(int inPresetIndex, int inKey, float inVelocity);
 	void note_off(int inPresetIndex, int inKey);
 	void note_off_all();
 
-    void channel_set_preset_index(int inChannel, int inPresetIndex);
-    int channel_set_preset_number(int inChannel, int inPresetNumber, int inDrums);
-    void channel_set_bank(int inChannel, int inBank);
-    int channel_set_bank_preset(int inChannel, int inBank, int inPresetNumber);
-    void channel_set_pan(int inChannel, float inPan);
-    void channel_set_volume(int inChannel, float inVolume);
-    void channel_set_pitchwheel(int inChannel, int inPitchWheel);
-    void channel_set_pitchrange(int inChannel, float inPitchRange);
-    void channel_set_tuning(int inChannel, float inTuning);
-    void channel_note_on(int inChannel, int inKey, float inVelocity);
-    void channel_note_off(int inChannel, int inKey);
-    void channel_note_off_all(int inChannel); // end with sustain and release
-    void channel_sounds_off_all(int inChannel); // end immediatly
-    void channel_midi_control(int inChannel, int inController, int inValue);
-    int channel_get_preset_index(int inChannel);
-    int channel_get_preset_bank(int inChannel);
-    int channel_get_preset_number(int inChannel);
-    float channel_get_pan(int inChannel);
-    float channel_get_volume(int inChannel);
-    int channel_get_pitchwheel(int inChannel);
-    float channel_get_pitchrange(int inChannel);
-    float channel_get_tuning(int inChannel);
-};
+	void channel_set_preset_index(int inChannel, int inPresetIndex);
+	int channel_set_preset_number(int inChannel, int inPresetNumber, int inDrums);
+	void channel_set_bank(int inChannel, int inBank);
+	int channel_set_bank_preset(int inChannel, int inBank, int inPresetNumber);
+	void channel_set_pan(int inChannel, float inPan);
+	void channel_set_volume(int inChannel, float inVolume);
+	void channel_set_pitchwheel(int inChannel, int inPitchWheel);
+	void channel_set_pitchrange(int inChannel, float inPitchRange);
+	void channel_set_tuning(int inChannel, float inTuning);
+	void channel_note_on(int inChannel, int inKey, float inVelocity);
+	void channel_note_off(int inChannel, int inKey);
+	void channel_note_off_all(int inChannel); // End with sustain and release.
+	void channel_sounds_off_all(int inChannel); // End immediately.
+	void channel_midi_control(int inChannel, int inController, int inValue);
+	int channel_get_preset_index(int inChannel);
+	int channel_get_preset_bank(int inChannel);
+	int channel_get_preset_number(int inChannel);
+	float channel_get_pan(int inChannel);
+	float channel_get_volume(int inChannel);
+	int channel_get_pitchwheel(int inChannel);
+	float channel_get_pitchrange(int inChannel);
+	float channel_get_tuning(int inChannel);
 
-#endif
+	~MidiPlayer();
+};
